4b9d8cb573afb3c5d768784e3eeb8c0e5d467f3a
Vinícius <viniciusvas2005@gmail.com>
2026-06-26 12:11:45 -0400

    style: Alterado o modelo 3d da logo

--- src/components/LogoNovare3d.tsx
+++ src/components/LogoNovare3d.tsx
@@ -10,13 +10,11 @@ interface LogoProps {
 }
 
 export function LogoNovare3d({ rotationState }: LogoProps) {
-  const { scene } = useGLTF('/models/logo3d.glb');
+  const { scene } = useGLTF('/models/logo3d(gradient - compressed).glb');
   const groupRef = useRef<THREE.Group>(null);
   const modelRef = useRef<THREE.Group>(null);
 
-  const gradientTexture = useTexture('/textures/logo-gradient.png');
-
-  const [scale, setScale] = useState(0.016);
+  const [scale, setScale] = useState(20.0);
 
   useFrame(() => {
     if (groupRef.current && rotationState?.current) {
@@ -27,9 +25,9 @@ export function LogoNovare3d({ rotationState }: LogoProps) {
   useEffect(() => {
     const escutarResize = () => {
       if (window.innerWidth < 768) {
-        setScale(0.01);
+        setScale(10.0);
       } else {
-        setScale(0.016);
+        setScale(20.0);
       }
     };
 
@@ -40,22 +38,18 @@ export function LogoNovare3d({ rotationState }: LogoProps) {
   }, []);
 
   useMemo(() => {
-    gradientTexture.wrapS = THREE.RepeatWrapping;
-    gradientTexture.wrapT = THREE.RepeatWrapping;
-
     scene.traverse((child) => {
       if ((child as THREE.Mesh).isMesh) {
         const mesh = child as THREE.Mesh;
 
-        mesh.material = new THREE.MeshStandardMaterial({
-          map: gradientTexture,
-          roughness: 0.1,
-          metalness: 0.2,
-        });
+        if (mesh.material && 'roughness' in mesh.material) {
+          const standardMaterial = mesh.material as THREE.MeshStandardMaterial;
+
+          standardMaterial.roughness = 0.2;
+        }
       }
     });
-  }, [scene, gradientTexture]);
-
+  }, [scene]);
   return (
     <Center>
       <group ref={groupRef}>
